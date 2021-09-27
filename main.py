121e80ff3e7719e4f28940f50e774f6c47cbec92
outer <outertoaster77@gmail.com>
2021-09-27 23:46:25 +1300

    fixed comments

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 import palette # load palette variables from file
 ## remember to prepend "palette." to the colour variable
 
-programIcon = pygame.image.load('tempicon.png') # set program name
-programName = "Blue Plane Game" # set program icon
+programIcon = pygame.image.load('tempicon.png') # set program icon
+programName = "Blue Plane Game" # set program name
 
 def setWindow():
 	WIDTH, HEIGHT = 512, 288 # set initial window size
