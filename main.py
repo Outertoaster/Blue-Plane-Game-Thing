b6509c04f5fe5e0a617f7571f94808f40700808e
outer <outertoaster77@gmail.com>
2021-09-27 22:05:05 +1300

    new script working in basic state

--- main.py
+++ main.py
@@ -3,22 +3,34 @@
 import pygame
 import os
 
+pygame.init()
+
+## reminder to self - stick entire DB32 palette into table so i dont have to
+## remember or keep copy/pasting rgb values from aseprite
+def setPal(): # set palette
+	global DARKBLUE
+	DARKBLUE = (91,110,225)
+
 def setWindow():
 	WIDTH, HEIGHT = 512, 288 # set initial window size
+	global WIN # make WIN global because its just easier to group stuff together in functions
 	WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 	pygame.display.set_caption("Blue Plane Game") # set window title
-
+setPal()
+setWindow() # Call window draw function
 def main():
-	setWindow() # Call window draw function
+	
 	
 	print("\nTesting, Testing!\n") # print in terminal if game starts
+	print(WIN)
 
 	run = True
-	while run:
+	while run: # main loop
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
-				
+		WIN.fill(DARKBLUE)
+		pygame.display.update()		
 	pygame.quit()
 
 
