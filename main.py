4ae23c55e56cf2965ae467ff2d0c638b691bd836
Outertoaster <outertoaster77@gmail.com>
2021-10-05 21:39:30 +1300

    commit to show brother how to use git

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python
 #main script - outers version
 import pygame
 import os
@@ -8,6 +8,9 @@ import palette # load palette variables from file
 
 programIcon = pygame.image.load('tempicon.png') # set program icon
 programName = "Blue Plane Game" # set program name
+FPS = 30	# set game framerate cap
+
+playerSpritesheet = pygame.image.load(os.path.join("assets", "player.png"))
 
 def setWindow():
 	WIDTH, HEIGHT = 512, 288 # set initial window size
@@ -16,19 +19,32 @@ def setWindow():
 	pygame.display.set_caption(programName) # set window title from var
 	pygame.display.set_icon(programIcon) # set app icon from var
 setWindow() # Call window draw function
-def main():
-	
-	
+
+def drawObjects():
+	WIN.blit(playerSpritesheet, (256, 144))
+
+def main():	
 	print("\nTesting, Testing!\n") # print in terminal if game starts
 	print(WIN)
-
+	
+	clock = pygame.time.Clock()
 	run = True
 	while run: # main loop
+		clock.tick(FPS)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
 		WIN.fill(palette.BLUE_1)
-		pygame.display.update()		
+
+		drawObjects() # draw objects
+
+
+
+
+
+
+
+		pygame.display.update()
 	pygame.quit()
 
 
